09947d7d9b276f2330171ffb2033156d3357602a
Vrutik Patwa <vrutikpatwa@gmail.com>
2025-07-14 20:00:05 +0530

    Finished Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
-function App (){
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
-  return <div>Hello</div>
+function App() {
+  return (
+    <div className="  bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
